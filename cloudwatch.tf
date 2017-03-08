475343f4e63bd55086e5c3ad6d228c39198cae9a
kumarp <p.kumar@kainos.com>
2017-03-08 14:28:38 +0000

    lambda warmup

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -20,7 +20,7 @@ resource "aws_cloudwatch_event_target" "MOTRWebHandler-WarmUpEventTarget" {
 
 resource "aws_cloudwatch_event_rule" "MOTR-WarmUpEventRule" {
   name                = "MOTR-${var.environment}-WarmUpEventRule"
-  description         = "MOTR WarmUp event rule"
+  description         = "MOTR WebHandler WarmUp (${var.environment}) event rule | Schedule: ${var.warmup_rate}"
   schedule_expression = "${var.warmup_rate}"
   is_enabled          = "${var.enable_warmup ? 1 : 0}"
 }
