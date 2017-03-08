79cd230af36e7dd32270ee465e7db2ba3f89606d
kumarp <p.kumar@kainos.com>
2017-03-08 13:57:13 +0000

    lambda warmup

--- events.tf
+++ events.tf
@@ -1,12 +1,12 @@
 resource "aws_cloudwatch_event_target" "MOTRWebHandler-WarmUpEventTarget" {
-  count     = "${var.enable_warmup ? 1 : 0}}"
+  count     = "${var.enable_warmup ? 1 : 0}"
   target_id = "MOTRWebHandler-WarmUpEventTarget"
   rule      = "${aws_cloudwatch_event_rule.MOTR-WarmUpEventRule.name}"
   arn       = "${aws_lambda_function.MotrWebHandler.arn}"
 }
 
 resource "aws_cloudwatch_event_rule" "MOTR-WarmUpEventRule" {
-  count               = "${var.enable_warmup ? 1 : 0}}"
+  count               = "${var.enable_warmup ? 1 : 0}"
   name                = "MOTR-WarmUpEventRule"
   description         = "MOTR WarmUp event rule"
   schedule_expression = "rate(1 minutes)"
