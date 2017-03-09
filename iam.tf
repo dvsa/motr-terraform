dd7e90875ec9cc90aaf4d5fa53a970478c455c9c
Sebastian Mincewicz <s.mincewicz@kainos.com>
2017-03-09 11:58:28 +0000

    S3 assets protection with OAI for CF

--- iam.tf
+++ iam.tf
@@ -1,4 +1,19 @@
 ####################################################################################################################################
+# S3
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    sid       = "Grant CF OAI access to static assets on S3"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${var.bucket_prefix}${var.environment}/assets/*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["${var.with_cloudfront ? "${aws_cloudfront_origin_access_identity.oai.iam_arn}" : "*"}"]
+    }
+  }
+}
+
+####################################################################################################################################
 # API GATEWAY
 
 # IAM role + policy that grants API Gateway permissons to assume roles
--- lambda.tf
+++ lambda.tf
@@ -18,7 +18,7 @@ resource "aws_lambda_function" "MotrWebHandler" {
       LOG_LEVEL                      = "${var.webapp_log_level}"
       REGION                         = "${var.aws_region}"
       STATIC_ASSETS_HASH             = "${var.static_assets_hash}"
-      STATIC_ASSETS_URL              = "https://s3-${var.aws_region}.amazonaws.com/${aws_s3_bucket.MOTRS3Bucket.bucket}/assets/"
+      STATIC_ASSETS_URL              = "${var.with_cloudfront ? "${var.base_url}/assets" : "https://s3-${var.aws_region}.amazonaws.com/${aws_s3_bucket.MOTRS3Bucket.bucket}/assets/"}"
       DB_TABLE_SUBSCRIPTION          = "motr-${var.environment}-subscription"
       DB_TABLE_PENDING_SUBSCRIPTION  = "motr-${var.environment}-pending_subscription"
       MOT_TEST_REMINDER_INFO_API_URI = "${var.mot_test_reminder_info_api_uri == "" ? "https://${aws_api_gateway_rest_api.MotrWeb.id}.execute-api.${var.aws_region}.amazonaws.com/${var.environment}/mot-test-reminder-mock/{registration}" : var.mot_test_reminder_info_api_uri}"
