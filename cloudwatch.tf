884ba4716760a02117efe006bf9867b693355816
Sebastian Mincewicz <s.mincewicz@kainos.com>
2017-04-10 13:58:11 +0100

    fixing metrics dependencies

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -45,6 +45,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrWebHandlerNotifyConfFailure_log
     namespace = "${var.project}-${var.environment}-MotrWebHandler-NotifyConfirmationFailure"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrWebHandler"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrWebHandlerColdStartUserExperience_log_metric_filter" {
@@ -56,6 +57,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrWebHandlerColdStartUserExperien
     namespace = "${var.project}-${var.environment}-MotrWebHandler-ColdStartUserExperience"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrWebHandler"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrWebHandlerSubscriptionActivationFailure_log_metric_filter" {
@@ -67,6 +69,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrWebHandlerSubscriptionActivatio
     namespace = "${var.project}-${var.environment}-MotrWebHandler-SubscriptionActivationFailure"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrWebHandler"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrWebHandlerPendingSubscriptionFailure_log_metric_filter" {
@@ -78,6 +81,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrWebHandlerPendingSubscriptionFa
     namespace = "${var.project}-${var.environment}-MotrWebHandler-PendingSubscriptionFailure"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrWebHandler"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrWebHandlerTradeAPIFailure_log_metric_filter" {
@@ -89,6 +93,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrWebHandlerTradeAPIFailure_log_m
     namespace = "${var.project}-${var.environment}-MotrWebHandler-TradeAPIFailure"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrWebHandler"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrWebHandler_MiscError_log_metric_filter" {
@@ -130,25 +135,27 @@ resource "aws_cloudwatch_event_target" "MotrLoaderStart" {
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrSubscriptionLoaderLoadingError_log_metric_filter" {
-  name = "MotrSubscriptionLoaderLoadingError_log_metric_filter"
-  pattern = "{ $.message = LOADING-ERROR }"
+  name           = "MotrSubscriptionLoaderLoadingError_log_metric_filter"
+  pattern        = "{ $.message = LOADING-ERROR }"
   log_group_name = "/aws/lambda/${aws_lambda_function.MotrSubscriptionLoader.function_name}"
   metric_transformation {
     name = "${var.project}-${var.environment}-MotrSubscriptionLoader-LoadingError"
     namespace = "${var.project}-${var.environment}-MotrSubscriptionLoader-LoadingError"
     value = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrSubscriptionLoader"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrSubscriptionLoaderLoadingSuccess_log_metric_filter" {
-  name = "MotrSubscriptionLoaderLoadingSuccess_log_metric_filter"
-  pattern = "{ $.message = LOADING-SUCCESS }"
+  name           = "MotrSubscriptionLoaderLoadingSuccess_log_metric_filter"
+  pattern        = "{ $.message = LOADING-SUCCESS }"
   log_group_name = "/aws/lambda/${aws_lambda_function.MotrSubscriptionLoader.function_name}"
   metric_transformation {
     name = "${var.project}-${var.environment}-MotrSubscriptionLoader-LoadingSuccess"
     namespace = "${var.project}-${var.environment}-MotrSubscriptionLoader-LoadingSuccess"
     value = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrSubscriptionLoader"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrSubscriptionLoader_MiscError_log_metric_filter" {
@@ -160,6 +167,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrSubscriptionLoader_MiscError_lo
     namespace = "${var.project}-${var.environment}-MotrSubscriptionLoader-MiscError"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrSubscriptionLoader"]
 }
 
 ####################################################################################################################################
@@ -198,6 +206,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrNotifierSubscriptionProcessingF
     namespace = "${var.project}-${var.environment}-MotrNotifier-SubscriptionProcessingFailed"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrNotifier"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrNotifierSubscriptionQueueItemRemovalFailed_log_metric_filter" {
@@ -209,6 +218,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrNotifierSubscriptionQueueItemRe
     namespace = "${var.project}-${var.environment}-MotrNotifier-SubscriptionQueueItemRemovalFailed"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrNotifier"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrNotifierVehicleDetailsRetrievalFailed_log_metric_filter" {
@@ -220,6 +230,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrNotifierVehicleDetailsRetrieval
     namespace = "${var.project}-${var.environment}-MotrNotifier-VehicleDetailsRetrievalFailed"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrNotifier"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrNotifierUnloadingTimedOut_log_metric_filter" {
@@ -231,6 +242,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrNotifierUnloadingTimedOut_log_m
     namespace = "${var.project}-${var.environment}-MotrNotifier-UnloadingTimedOut"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrNotifier"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MotrNotifier_MiscError_log_metric_filter" {
@@ -242,6 +254,7 @@ resource "aws_cloudwatch_log_metric_filter" "MotrNotifier_MiscError_log_metric_f
     namespace = "${var.project}-${var.environment}-MotrNotifier-MiscError"
     value     = "1"
   }
+  depends_on     = ["aws_cloudwatch_log_group.MotrNotifier"]
 }
 
 ####################################################################################################################################
